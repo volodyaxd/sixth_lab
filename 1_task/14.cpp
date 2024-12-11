426310059c4f14183673b07e35e9d29198dfe16c
reflection <70287274+volodyaxd@users.noreply.github.com>
2024-12-11 23:25:09 +0500

    -

--- 1_task/14.cpp
+++ 1_task/14.cpp
@@ -5,7 +5,9 @@
 void keepFirstOccurrences(std::list<int>& L) {
     std::set<int> seen;
     auto it = L.begin();
+
     while (it != L.end()) {
+        
         if (!seen.insert(*it).second) {
             it = L.erase(it);
         } else {
@@ -15,9 +17,26 @@ void keepFirstOccurrences(std::list<int>& L) {
 }
 
 int main() {
-    std::list
-    <int> L = {1, 2, 2, 3, 3, 4};
-        keepFirstOccurrences(L);
-        for (int x : L) std::cout << x << " ";
-        return 0;
+    std::list<int> L;
+    int n, value;
+
+    std::cout << "Введите количество элементов в списке: ";
+    std::cin >> n;
+
+    std::cout << "Введите " << n << " элементов:\n";
+    for (int i = 0; i < n; ++i) {
+        std::cin >> value;
+        L.push_back(value);
     }
+
+    keepFirstOccurrences(L);
+
+    
+    std::cout << "Список после удаления дубликатов: ";
+    for (int x : L) {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
